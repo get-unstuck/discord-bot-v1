aebbfefcfd8d335af303e12224194861b44c73d8
Deveshb15 <deveshb583@gmail.com>
2022-11-05 20:22:44 +0530

    chore: process only if replied to a question and don't process if it's replied to bot

--- index.js
+++ index.js
@@ -12,43 +12,53 @@ const client = new Client({
 });
 
 const getTags = (message) => {
-    let tags = []
-    message.split(" ").forEach((word) => {
-        if(word.includes("#")) {
-            tags.push(word)
-        }
-    })
+	let tags = [];
+	message.split(" ").forEach((word) => {
+		if (word.includes("#")) {
+			tags.push(word);
+		}
+	});
 
-    return tags.join(" ")
-} 
+	return tags.join(" ");
+};
 
 client.on("ready", () => {
 	console.log(`Bot is online ${client.user.tag}!`);
 });
 
 client.on("messageCreate", async (msg) => {
-    try {
-        // console.log(msg)
-        if (msg.author.bot) return;
+	try {
+		// console.log(msg.mentions)
+		if (msg.author.bot) return;
 
-        // Only reply when tagged
-        if(msg.mentions.has(client.user.id)) {
-            // Only process/add to db when tags are added
-            if (msg.content.includes('#')) {
-                const repliedToMessage = await msg.channel.messages.fetch(msg.reference.messageId)
-    
-                // Get the Tags
-                const tags = getTags(msg.content)
-        
-                msg.reply(`Question: ${repliedToMessage} \ntags: ${tags}`)
-        
-            } else {
-                msg.reply("sorry can't process this!");
-            }
-        }
-    } catch(err) {
-        console.log("ERROR ", err)
-    }
+		/**
+		 * Only reply when tagged
+		 * Process only if it's a reply to a question
+		 * Don't reply if it's replied to the bot itself
+		 */
+
+		if (
+			msg.mentions.has(client.user.id) &&
+			msg.reference &&
+			!(msg.mentions.repliedUser.id === client.user.id)
+		) {
+			// Only process/add to db when tags are added
+			if (msg.content.includes("#")) {
+				const repliedToMessage = await msg.channel.messages.fetch(
+					msg.reference.messageId
+				);
+
+				// Get the Tags
+				const tags = getTags(msg.content);
+
+				msg.reply(`Question: ${repliedToMessage} \ntags: ${tags}`);
+			} else {
+				msg.reply("sorry can't process this!");
+			}
+		}
+	} catch (err) {
+		console.log("ERROR ", err);
+	}
 });
 
 client.login(process.env.TOKEN);
